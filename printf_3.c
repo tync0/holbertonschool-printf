7b3a4a0e8290af7d10c4e92d07ed4cc4c0254d7c
tync0 <isgenderlituncay2004@gmail.com>
2023-11-27 05:25:42 -0800

    1

--- printf_3.c
+++ printf_3.c
@@ -19,6 +19,8 @@ int _printf(const char *format, ...)
 	char s;
 
 	va_start(ptr, format);
+	if (format == NULL)
+		return (size);
 	while (format[i])
 	{
 		if (format[i] != '%')
@@ -28,21 +30,30 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			if (format[i + 1] == 'c')
+			if (format[i + 1] && format[i + 1] == 'c')
 			{
 				_putchar(va_arg(ptr, char));
 				size++;
 				i++;
 			}
-			else if (format[i + 1] == 's')
+			else if (format[i + 1] && format[i + 1] == 's')
 			{
 				str = va_arg(ptr, char *);
-				size += write(1, str, strlen(str));
+				if (str == NULL)
+					size += write(1, "(null)", strlen("(null)"));
+				else
+					size += write(1, str, strlen(str));
 				i++;
+			}
+			else if (format[i + 1])
+			{
+				_putchar(format[i]);
+				size++;
 			}
 		}
 		i++;
 	}
+	va_end(ptr);
 
 	return (size);
 }
